330e262bbc0e2daca1444808fbbfff908138a054
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-11-23 00:02:14 +0700

    feat: added swagger

---
+++ src/main/java/com/robert/codingchallenge/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.robert.codingchallenge.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public ModelResolver modelResolver(ObjectMapper objectMapper) {
+		return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+	}
+
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+				.title("City Suggestions API")
+				.version("1.0")
+				.description("City Suggestions API provides endpoint that suggests cities based on queries.");
+
+		return new OpenAPI()
+				.info(info);
+	}
+}
--- src/main/java/com/robert/codingchallenge/controller/CityController.java
+++ src/main/java/com/robert/codingchallenge/controller/CityController.java
@@ -1,8 +1,16 @@
 package com.robert.codingchallenge.controller;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/city")
 public class CityController {
+
+	@GetMapping("/suggestions")
+	public ResponseEntity<String> getSuggestions() {
+		String response = "Hello World!";
+		return ResponseEntity.ok(response);
+	}
 }
